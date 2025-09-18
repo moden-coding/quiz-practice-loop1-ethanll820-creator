917510d3de86305b5fc4f2f56afeb1af773706fb
Ethan <ethanll820@gmail.com>
2025-09-18 19:42:02 -0400

    done

--- src/App.java
+++ src/App.java
@@ -10,7 +10,25 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
-
+        int total = 0;
+        int count = 1;
+        int oddnumbers = 0;
+        int evennumbers = 0;
+        
+        while (true) {
+            System.out.println("Give a number");
+           int numbers = Integer.valueOf(scanner.nextLine());
+            if (numbers == -1) {
+                break;
+            }else if (numbers % 2 == 0) {
+                System.out.println("The number is even");
+                evennumbers = evennumbers + 1;
+            }else {
+                System.out.println("The number is odd");
+                oddnumbers = oddnumbers + 1;
+            }
 
     }
+    System.out.println("Even numbers: " + evennumbers + "," + " Odd numbers: " + oddnumbers);
+}
 }
